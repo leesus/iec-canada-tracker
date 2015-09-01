39d2540a18260f6ffcc0b379b47b6608ab2e8455
leesus <lee@lee-ellam.com>
2015-09-01 22:23:23 +0100

    add UNTESTED user auth etc

--- server/index.js
+++ server/index.js
@@ -2,15 +2,61 @@
 
 import express from 'express';
 import mongoose from 'mongoose';
+import path from 'path';
+import favicon from 'serve-favicon';
+import logger from 'morgan';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+import compress from 'compression';
+import session from 'express-session';
+import validator from 'express-validator';
+import Store from 'connect-mongo';
+
+// Routes
+import router from './routes';
+
+// Config
+import passportConfig from './config/passport';
 import config from '../config/secrets';
 
 let secrets = config[process.env.NODE_ENV || 'development'];
 let app = express();
+let MongoStore = Store({ session: session });
 
 // Connect to mongo
 mongoose.connect(secrets.db);
 mongoose.connection.on('error', () => console.error('MongoDB Connection Error. Make sure MongoDB is running.'));
 mongoose.connection.on('disconnect', () => mongoose.connect(secrets.db));
+
+// middleware
+// view engine setup
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade')
+app.use(compress());
+app.use(validator());
+//app.use(favicon(path.join(__dirname, '/favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(session({
+    secret: secrets.sessionSecret,
+    store: new MongoStore({
+        url: secrets.db,
+        autoReconnect: true,
+        collection: 'session'
+    }),
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static(path.join(__dirname, './public')));
+app.use((req, res, next) => {
+    if (req.user) res.cookie('user', JSON.stringify(req.user));
+    next();
+});
 
 import agenda from '../lib/agenda';
 import agendaUI from 'agenda-ui';
---
+++ server/models/user.js
@@ -0,0 +1,41 @@
+'use strict';
+
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+
+let Schema = mongoose.Schema;
+
+let UserSchema = new Schema({
+  name: String
+  email: [{ type: String, required: true, unique: true }],
+  password: String,
+  created_date: { type: Date, 'default': Date.now }
+});
+
+UserSchema.pre('save', function(next) {
+  let user = this;
+
+  if (!user.isModified('password')) return next();
+  
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) return next(err);
+
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if (err) return next(err);
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+UserSchema.methods.comparePassword = function(password, fn) {
+  bcrypt.compare(password, this.password, (err, isMatch) => {
+    if (err) return fn(err);
+
+    fn(null, isMatch);
+  });
+};
+
+UserSchema.index({ name: 'text', email: 'text' });
+
+export default mongoose.model('User', UserSchema);
